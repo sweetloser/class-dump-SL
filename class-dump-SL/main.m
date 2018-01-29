1e67e16ec173a8925ce7fc6caa8aaac1f8564020
SweetLoser <sl@SweetLoserdeMacBook-Pro.local>
2018-01-29 23:42:17 +0800

    g

---
+++ class-dump-SL/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  class-dump-SL
+//
+//  Created by SL on 2018/1/29.
+//  Copyright © 2018年 SweetLoser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
